131249d30ca48489b412377486a73c0fed928642
Christian Helle <crh@delegate.dk>
2020-01-21 20:37:23 +0100

    Create Exception extension methods

---
+++ src/AppCenterExtensions/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.AppCenter.Crashes;
+
+namespace ChristianHelle.DeveloperTools.AppCenterExtensions.Extensions
+{
+    [ExcludeFromCodeCoverage]
+    public static class ExceptionExtensions
+    {
+        public static void Report(
+            this Exception exception,
+            IDictionary<string, string> properties = null)
+        {
+            try
+            {
+                Crashes.TrackError(exception, properties);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.ToString());
+                Crashes.TrackError(e);
+            }
+        }
+
+        public static void Log(this Exception exception)
+            => exception.Report();
+    }
+}
