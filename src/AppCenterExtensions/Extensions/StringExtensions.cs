a4de61bb4abf0251fdddc4959947572da377c7e0
Christian Helle <crh@delegate.dk>
2020-01-21 20:39:06 +0100

    Create string extension method for retrieving an analytics friendly event name from a class name

---
+++ src/AppCenterExtensions/Extensions/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ChristianHelle.DeveloperTools.AppCenterExtensions.Extensions
+{
+    public static class StringExtension
+    {
+        public static string ToTrackingEventName(this string input)
+            => Regex
+                .Replace(
+                    input,
+                    "([A-Z])",
+                    " $1",
+                    RegexOptions.Compiled)
+                .Replace("Command", string.Empty)
+                .Replace("View", string.Empty)
+                .Replace("Model", string.Empty)
+                .Replace("Page", string.Empty)
+                .Replace("Async", string.Empty)
+                .Trim();
+    }
+}
