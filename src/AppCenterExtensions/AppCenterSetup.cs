1db440d10e69d292678ebd5d45b81349131f4054
Christian Helle <crh@delegate.dk>
2020-01-23 21:48:50 +0100

    Extract interface from AppCenterSetup and implement lazy singleton

--- src/AppCenterExtensions/AppCenterSetup.cs
+++ src/AppCenterExtensions/AppCenterSetup.cs
@@ -1,18 +1,35 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using Microsoft.AppCenter;
 
 namespace ChristianHelle.DeveloperTools.AppCenterExtensions
 {
-    public static class AppCenterSetup
+    public interface IAppCenterSetup
     {
-        public static async Task UseAnonymousUserIdAsync()
+        Task UseAnonymousUserIdAsync();
+        Task<string> GetSupportKeyAsync();
+        
+        string AppCenterSdkVersion { get; }
+        Task<Guid?> GetAppCenterInstallIdAsync();
+    }
+
+    [ExcludeFromCodeCoverage]
+    public sealed class AppCenterSetup : IAppCenterSetup
+    {
+        private static readonly Lazy<AppCenterSetup> lazyInstance = new Lazy<AppCenterSetup>();
+        public static AppCenterSetup Instance => lazyInstance.Value;
+
+        public async Task UseAnonymousUserIdAsync()
             => AppCenter.SetUserId(
                 await GetSupportKeyAsync());
 
-        public static async Task<string> GetSupportKeyAsync()
+        public async Task<string> GetSupportKeyAsync()
             => (await AppCenter.GetInstallIdAsync() ?? Guid.NewGuid())
                 .ToString()
                 .Substring(0, 8);
+
+        public string AppCenterSdkVersion => AppCenter.SdkVersion;
+        public Task<Guid?> GetAppCenterInstallIdAsync() => AppCenter.GetInstallIdAsync();
     }
 }
