1270d0bc07772df189077f578a2c8f60c852a516
Christian Helle <crh@delegate.dk>
2020-02-04 23:00:09 +0100

    Add XML documentation for IAnalytics and its implementation

--- src/AppCenterExtensions/AppCenterAnalytics.cs
+++ src/AppCenterExtensions/AppCenterAnalytics.cs
@@ -4,11 +4,20 @@ using Microsoft.AppCenter.Analytics;
 
 namespace AppCenterExtensions
 {
+    /// <inheritdoc />
     [ExcludeFromCodeCoverage]
     public class AppCenterAnalytics : IAnalytics
     {
+        /// <summary>
+        /// Singleton default implementation
+        /// </summary>
         public static IAnalytics Instance { get; } = new AppCenterAnalytics();
 
+        /// <summary>
+        /// Track a custom event
+        /// </summary>
+        /// <param name="name">Event name</param>
+        /// <param name="properties">Custom properties to include in event</param>
         public void TrackEvent(string name, IDictionary<string, string> properties = null) 
             => Analytics.TrackEvent(name, properties);
     }
--- src/AppCenterExtensions/IAnalytics.cs
+++ src/AppCenterExtensions/IAnalytics.cs
@@ -2,8 +2,16 @@
 
 namespace AppCenterExtensions
 {
+    /// <summary>
+    /// An interface clone of AppCenter Analytics
+    /// </summary>
     public interface IAnalytics
     {
+        /// <summary>
+        /// Track a custom event
+        /// </summary>
+        /// <param name="name">Event name</param>
+        /// <param name="properties">Custom properties to include in event</param>
         void TrackEvent(string name, IDictionary<string, string> properties = null);
     }
 }
