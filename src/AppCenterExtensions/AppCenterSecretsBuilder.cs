d079f022d5d93569a663da9ea244bd0492329e91
Christian Helle <crh@delegate.dk>
2020-02-04 23:00:53 +0100

    Add XML documentation for AppCenterSecretsBuilder

--- src/AppCenterExtensions/AppCenterSecretsBuilder.cs
+++ src/AppCenterExtensions/AppCenterSecretsBuilder.cs
@@ -1,26 +1,61 @@
 using System;
+using System.Text;
 
 namespace AppCenterExtensions
 {
+    /// <summary>
+    /// A fluent API for building the AppCenter API secrets
+    /// </summary>
     public class AppCenterSecretsBuilder
     {
         private string appleSecret;
         private string androidSecret;
 
+        /// <summary>
+        /// Set the API secret for the iOS app
+        /// </summary>
+        /// <param name="secret">iOS secret</param>
+        /// <returns>Returns self (Fluent API)</returns>
         public AppCenterSecretsBuilder SetAppleSecret(string secret)
         {
             appleSecret = secret ?? throw new ArgumentNullException(nameof(secret));
             return this;
         }
 
+        /// <summary>
+        /// Sets the API secret for the Android app
+        /// </summary>
+        /// <param name="secret">Android secret</param>
+        /// <returns>Returns self (Fluent API)</returns>
         public AppCenterSecretsBuilder SetAndroidSecret(string secret)
         {
             androidSecret = secret ?? throw new ArgumentNullException(nameof(secret));
             return this;
         }
 
-        public string Build() => $"ios={appleSecret};android={androidSecret}";
+        /// <summary>
+        /// Builds a single AppCenter API secret string
+        /// </summary>
+        /// <returns>This returns a string like ios={secret};android={secret}</returns>
+        public string Build()
+        {
+            var builder = new StringBuilder();
+            
+            if (!string.IsNullOrWhiteSpace(appleSecret))
+                builder.Append($"ios={appleSecret};");
+            
+            if (!string.IsNullOrWhiteSpace(androidSecret))
+                builder.Append($"android={androidSecret}");
+            
+            return builder.ToString();
+        }
 
+        /// <summary>
+        /// Builds a single AppCenter API secret string
+        /// </summary>
+        /// <param name="appleSecret">iOS secret</param>
+        /// <param name="androidSecret">Android secret</param>
+        /// <returns>This returns a string like ios={secret};android={secret}</returns>
         public static string Build(string appleSecret, string androidSecret)
             => new AppCenterSecretsBuilder()
                 .SetAppleSecret(appleSecret)
