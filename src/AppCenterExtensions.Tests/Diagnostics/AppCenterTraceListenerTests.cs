187d1d91be1590ffcd70075aecbd5fe68f8199da
Christian Helle <crh@delegate.dk>
2020-02-04 12:47:42 +0100

    Implement AppCenter TraceListener

---
+++ src/AppCenterExtensions.Tests/Diagnostics/AppCenterTraceListenerTests.cs
@@ -0,0 +1,161 @@
+using System;
+using System.Collections.Generic;
+using ChristianHelle.DeveloperTools.AppCenterExtensions.Diagnostics;
+using ChristianHelle.DeveloperTools.AppCenterExtensions.Tests.Infrastructure;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace ChristianHelle.DeveloperTools.AppCenterExtensions.Tests.Diagnostics
+{
+    public class AppCenterTraceListenerTests
+    {
+        [Theory, AutoMoqData]
+        public void Constructor_Sets_Analytics(IAnalytics analytics, ICrashes crashes)
+            => new AppCenterTraceListener(analytics, crashes)
+                .Analytics
+                .Should()
+                .Be(analytics);
+
+        [Theory, AutoMoqData]
+        public void Constructor_Sets_Crashes(IAnalytics analytics, ICrashes crashes)
+            => new AppCenterTraceListener(analytics, crashes)
+                .Crashes
+                .Should()
+                .Be(crashes);
+
+        [Theory, AutoMoqData]
+        public void Write_Null_Does_Nothing(
+            AppCenterTraceListener sut)
+        {
+            sut.Write(null as object);
+            AssertDoesNothing(sut);
+        }
+
+        [Theory, AutoMoqData]
+        public void WriteLine_Null_Does_Nothing(
+            AppCenterTraceListener sut)
+        {
+            sut.WriteLine(null as object);
+            AssertDoesNothing(sut);
+        }
+
+        [Theory, AutoMoqData]
+        public void Write_With_Category_Null_Does_Nothing(
+            AppCenterTraceListener sut,
+            string category)
+        {
+            sut.Write(null as object, category);
+            AssertDoesNothing(sut);
+        }
+
+        [Theory, AutoMoqData]
+        public void WriteLine_With_Category_Null_Does_Nothing(
+            AppCenterTraceListener sut,
+            string category)
+        {
+            sut.WriteLine(null as object, category);
+            AssertDoesNothing(sut);
+        }
+
+        private static void AssertDoesNothing(AppCenterTraceListener sut)
+        {
+            Mock.Get(sut.Crashes)
+                .Verify(
+                    c => c.TrackError(
+                        It.IsAny<Exception>(),
+                        It.IsAny<IDictionary<string, string>>()),
+                    Times.Never);
+
+            Mock.Get(sut.Analytics)
+                .Verify(c => c.TrackEvent(
+                        It.IsAny<string>(),
+                        It.IsAny<IDictionary<string, string>>()),
+                    Times.Never);
+        }
+
+        [Theory, AutoMoqData]
+        public void Write_Exception_Invokes_Crashes_TrackError(
+            AppCenterTraceListener sut,
+            Exception exception)
+        {
+            sut.Write(exception);
+            Mock.Get(sut.Crashes)
+                .Verify(c => c.TrackError(exception, null));
+        }
+
+        [Theory, AutoMoqData]
+        public void WriteLine_Exception_Invokes_Crashes_TrackError(
+            AppCenterTraceListener sut,
+            Exception exception)
+        {
+            sut.WriteLine(exception);
+            Mock.Get(sut.Crashes)
+                .Verify(c => c.TrackError(exception, null));
+        }
+
+        [Theory, AutoMoqData]
+        public void Write_With_Category_Exception_Invokes_Crashes_TrackError(
+            AppCenterTraceListener sut,
+            Exception exception,
+            string category)
+        {
+            sut.Write(exception, category);
+            Mock.Get(sut.Crashes)
+                .Verify(c => c.TrackError(exception, null));
+        }
+
+        [Theory, AutoMoqData]
+        public void WriteLine_With_Category_Exception_Invokes_Crashes_TrackError(
+            AppCenterTraceListener sut,
+            Exception exception,
+            string category)
+        {
+            sut.WriteLine(exception, category);
+            Mock.Get(sut.Crashes)
+                .Verify(c => c.TrackError(exception, null));
+        }
+
+        [Theory, AutoMoqData]
+        public void Write_AnalyticsEvent_Invokes_Analytics_TrackEvent(
+            AppCenterTraceListener sut,
+            AnalyticsEvent analyticsEvent)
+        {
+            sut.Write(analyticsEvent);
+            Mock.Get(sut.Analytics)
+                .Verify(c => c.TrackEvent(analyticsEvent.EventName, analyticsEvent.Properties));
+        }
+
+        [Theory, AutoMoqData]
+        public void Write_With_Category_AnalyticsEvent_Invokes_Analytics_TrackEvent(
+            AppCenterTraceListener sut,
+            AnalyticsEvent analyticsEvent,
+            string category)
+        {
+            sut.Write(analyticsEvent, category);
+            Mock.Get(sut.Analytics)
+                .Verify(c => c.TrackEvent(analyticsEvent.EventName, analyticsEvent.Properties));
+        }
+
+        [Theory, AutoMoqData]
+        public void WriteLine_AnalyticsEvent_Invokes_Analytics_TrackEvent(
+            AppCenterTraceListener sut,
+            AnalyticsEvent analyticsEvent)
+        {
+            sut.WriteLine(analyticsEvent);
+            Mock.Get(sut.Analytics)
+                .Verify(c => c.TrackEvent(analyticsEvent.EventName, analyticsEvent.Properties));
+        }
+
+        [Theory, AutoMoqData]
+        public void WriteLine_With_Category_AnalyticsEvent_Invokes_Analytics_TrackEvent(
+            AppCenterTraceListener sut,
+            AnalyticsEvent analyticsEvent,
+            string category)
+        {
+            sut.WriteLine(analyticsEvent, category);
+            Mock.Get(sut.Analytics)
+                .Verify(c => c.TrackEvent(analyticsEvent.EventName, analyticsEvent.Properties));
+        }
+    }
+}
---
+++ src/AppCenterExtensions/Diagnostics/AppCenterTraceListener.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ChristianHelle.DeveloperTools.AppCenterExtensions.Diagnostics
+{
+    public class AppCenterTraceListener : TraceListener
+    {
+        public AppCenterTraceListener(IAnalytics analytics = null, ICrashes crashes = null)
+        {
+            Analytics = analytics ?? AppCenterAnalytics.Instance;
+            Crashes = crashes ?? AppCenterCrashes.Instance;
+        }
+
+        public IAnalytics Analytics { get; }
+        public ICrashes Crashes { get; }
+
+        [ExcludeFromCodeCoverage]
+        public override void Write(string message)
+        {
+        }
+
+        [ExcludeFromCodeCoverage]
+        public override void WriteLine(string message)
+        {
+        }
+
+        public override void Write(object o)
+            => WriteInternal(o);
+
+        public override void Write(object o, string category)
+            => WriteInternal(o);
+
+        public override void WriteLine(object o)
+            => WriteInternal(o);
+
+        public override void WriteLine(object o, string category)
+            => WriteInternal(o);
+
+        private void WriteInternal(object o)
+        {
+            if (o == null)
+                return;
+
+            switch (o)
+            {
+                case Exception exception:
+                    Crashes.TrackError(exception);
+                    break;
+
+                case AnalyticsEvent analyticsEvent:
+                    Analytics.TrackEvent(analyticsEvent.EventName, analyticsEvent.Properties);
+                    break;
+            }
+        }
+    }
+}
