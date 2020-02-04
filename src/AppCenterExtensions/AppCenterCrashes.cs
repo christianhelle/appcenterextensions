5cc4d209265e3a3a954fa635495da3f47415117d
Christian Helle <crh@delegate.dk>
2020-02-04 23:00:21 +0100

    Add XML documentation for ICrashes and its implementation

--- src/AppCenterExtensions/AppCenterCrashes.cs
+++ src/AppCenterExtensions/AppCenterCrashes.cs
@@ -5,11 +5,21 @@ using Microsoft.AppCenter.Crashes;
 
 namespace AppCenterExtensions
 {
+    /// <inheritdoc />
     [ExcludeFromCodeCoverage]
     public class AppCenterCrashes : ICrashes
     {
+        /// <summary>
+        /// Singleton default implementation
+        /// </summary>
         public static ICrashes Instance { get; } = new AppCenterCrashes();
-        
+
+        /// <summary>
+        /// Track a handled error
+        /// </summary>
+        /// <param name="exception">Exception thrown</param>
+        /// <param name="properties">Custom properties to attach to the error report</param>
+        /// <param name="attachments">Custom attachments</param>
         public void TrackError(
             Exception exception,
             IDictionary<string, string> properties = null,
--- src/AppCenterExtensions/ICrashes.cs
+++ src/AppCenterExtensions/ICrashes.cs
@@ -4,8 +4,17 @@ using Microsoft.AppCenter.Crashes;
 
 namespace AppCenterExtensions
 {
+    /// <summary>
+    /// An interface clone of AppCenter Crashes
+    /// </summary>
     public interface ICrashes
     {
+        /// <summary>
+        /// Track a handled error
+        /// </summary>
+        /// <param name="exception">Exception thrown</param>
+        /// <param name="properties">Custom properties to attach to the error report</param>
+        /// <param name="attachments">Custom attachments</param>
         void TrackError(
             Exception exception,
             IDictionary<string, string> properties = null,
