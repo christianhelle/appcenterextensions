cf60742644a238bb74f55b15e517e0c4916c83df
Christian Helle <crh@delegate.dk>
2020-02-05 11:14:50 +0100

    Add XML documentation for types under the Diagnostics namespace

--- src/AppCenterExtensions/Diagnostics/AnalyticsEvent.cs
+++ src/AppCenterExtensions/Diagnostics/AnalyticsEvent.cs
@@ -3,8 +3,18 @@ using System.Collections.Generic;
 
 namespace AppCenterExtensions.Diagnostics
 {
+    /// <summary>
+    /// This type must be used as the object argument with System.Diagnostics.Trace.Write()
+    /// to log to AppCenter Analytics when the AppCenterTraceListener is added to the
+    /// current Trace Listeners
+    /// </summary>
     public class AnalyticsEvent
     {
+        /// <summary>
+        /// Creates an instance of AnalyticsEvent
+        /// </summary>
+        /// <param name="eventName">Event name that describes the event occurring (ex: Logout Button Tapped)</param>
+        /// <param name="properties">Custom properties to attach to the analytics data (ex: {"Current Item",CurrentItem.Name})</param>
         public AnalyticsEvent(
             string eventName,
             IDictionary<string, string> properties = null)
@@ -13,7 +23,14 @@ namespace AppCenterExtensions.Diagnostics
             Properties = properties;
         }
 
+        /// <summary>
+        /// Gets the event name that describes the event occurring (ex: Logout Button Tapped)
+        /// </summary>
         public string EventName { get; }
-        public IDictionary<string,string> Properties { get; }
+
+        /// <summary>
+        /// Gets the custom properties to attach to the analytics data (ex: {"Current Item",CurrentItem.Name})
+        /// </summary>
+        public IDictionary<string, string> Properties { get; }
     }
 }
--- src/AppCenterExtensions/Diagnostics/AppCenterTraceListener.cs
+++ src/AppCenterExtensions/Diagnostics/AppCenterTraceListener.cs
@@ -4,36 +4,66 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace AppCenterExtensions.Diagnostics
 {
+    /// <summary>
+    /// A Trace Listener implementation that automatically sends Exceptions
+    /// to AppCenter Diagnostics and instances of AnalyticsEvent to AppCenter Analytics
+    /// </summary>
     public class AppCenterTraceListener : TraceListener
     {
+        /// <summary>
+        /// Creates an instance of AppCenterTraceListener
+        /// </summary>
+        /// <param name="analytics">Keep this as NULL to use the default implementation. This is only exposed for unit testing purposes</param>
+        /// <param name="crashes">Keep this as NULL to use the default implementation. This is only exposed for unit testing purposes</param>
         public AppCenterTraceListener(IAnalytics analytics = null, ICrashes crashes = null)
         {
             Analytics = analytics ?? AppCenterAnalytics.Instance;
             Crashes = crashes ?? AppCenterCrashes.Instance;
         }
 
+        /// <summary>
+        /// Gets the IAnalytics implementation in use
+        /// </summary>
         public IAnalytics Analytics { get; }
+
+        /// <summary>
+        /// Gets the ICrashes implementation in use
+        /// </summary>
         public ICrashes Crashes { get; }
 
+        /// <summary>When overridden in a derived class, writes the specified message to the listener you create in the derived class.</summary>
+        /// <param name="message">A message to write.</param>
         [ExcludeFromCodeCoverage]
         public override void Write(string message)
         {
         }
 
+        /// <summary>Writes the value of the object's <see cref="M:System.Object.ToString"></see> method to the listener you create when you implement the <see cref="T:System.Diagnostics.TraceListener"></see> class.</summary>
+        /// <param name="o">An <see cref="T:System.Object"></see> whose fully qualified class name you want to write.</param>
         public override void Write(object o)
             => WriteInternal(o);
 
+        /// <summary>Writes a category name and the value of the object's <see cref="M:System.Object.ToString"></see> method to the listener you create when you implement the <see cref="T:System.Diagnostics.TraceListener"></see> class.</summary>
+        /// <param name="o">An <see cref="T:System.Object"></see> whose fully qualified class name you want to write.</param>
+        /// <param name="category">A category name used to organize the output.</param>
         public override void Write(object o, string category)
             => WriteInternal(o);
 
+        /// <summary>When overridden in a derived class, writes a message to the listener you create in the derived class, followed by a line terminator.</summary>
+        /// <param name="message">A message to write.</param>
         [ExcludeFromCodeCoverage]
         public override void WriteLine(string message)
         {
         }
 
+        /// <summary>Writes the value of the object's <see cref="M:System.Object.ToString"></see> method to the listener you create when you implement the <see cref="T:System.Diagnostics.TraceListener"></see> class, followed by a line terminator.</summary>
+        /// <param name="o">An <see cref="T:System.Object"></see> whose fully qualified class name you want to write.</param>
         public override void WriteLine(object o)
             => WriteInternal(o);
 
+        /// <summary>Writes a category name and the value of the object's <see cref="M:System.Object.ToString"></see> method to the listener you create when you implement the <see cref="T:System.Diagnostics.TraceListener"></see> class, followed by a line terminator.</summary>
+        /// <param name="o">An <see cref="T:System.Object"></see> whose fully qualified class name you want to write.</param>
+        /// <param name="category">A category name used to organize the output.</param>
         public override void WriteLine(object o, string category)
             => WriteInternal(o);
 
