f8e481c8e2d1cd9a7195c40687b221c3583b8946
Christian Helle <crh@delegate.dk>
2020-02-05 12:27:29 +0100

    Add XML documentation for ActionExtensions

--- src/AppCenterExtensions/Extensions/ActionExtensions.cs
+++ src/AppCenterExtensions/Extensions/ActionExtensions.cs
@@ -3,8 +3,18 @@ using System.Diagnostics;
 
 namespace AppCenterExtensions.Extensions
 {
+    /// <summary>
+    /// Exposes extension methods for the Action class
+    /// </summary>
     public static class ActionExtensions
     {
+        /// <summary>
+        /// Safely invoke an Action and swallow the result
+        /// or provide a callback method that will be invoked
+        /// in the case of an exception being thrown
+        /// </summary>
+        /// <param name="action">Callback method</param>
+        /// <param name="onError">Callback method invoked in the case of an exception being thrown</param>
         public static void SafeInvoke(
             this Action action, 
             Action<Exception> onError = null)
