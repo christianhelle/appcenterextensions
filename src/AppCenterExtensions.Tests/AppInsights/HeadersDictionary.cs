b0b77d06f72cc6bda44523bd2203835e33f47fb3
Christian Resma Helle <christian.helle@outlook.com>
2020-02-28 23:29:47 +0100

    Implement ITelemetryInitializer AppCenterTelemetryInitializer

---
+++ src/AppCenterExtensions.Tests/AppInsights/HeadersDictionary.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+using System.Collections.Generic;
+
+namespace AppCenterExtensions.Tests.AppInsights
+{
+    public class HeadersDictionary : Dictionary<string, StringValues>, IHeaderDictionary
+    {
+        public long? ContentLength
+        {
+            get => throw new System.NotImplementedException();
+            set => throw new System.NotImplementedException();
+        }
+    }
+}
