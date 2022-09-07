7106dd25eb4d2a5e5fc8796e477161cb43b5e41f
Christian Helle <christian.helle@outlook.com>
2022-09-07 23:47:27 +0200

    Add support for UWP secrets

--- src/AppCenterExtensions.Tests/AppCenterSecretsBuilderTests.cs
+++ src/AppCenterExtensions.Tests/AppCenterSecretsBuilderTests.cs
@@ -20,13 +20,21 @@ namespace AppCenterExtensions.Tests
                 .ThrowExactly<ArgumentNullException>();
 
         [Theory, AutoMoqData]
+        public void Requires_UwpSecret(AppCenterSecretsBuilder sut)
+            => new Action(() => sut.SetUwpSecret(null))
+                .Should()
+                .ThrowExactly<ArgumentNullException>();
+
+        [Theory, AutoMoqData]
         public void Build_Returns_NotNull(
             AppCenterSecretsBuilder sut,
             string appleSecret,
-            string androidSecret)
+            string androidSecret,
+            string uwpSecret)
             => sut
                 .SetAppleSecret(appleSecret)
                 .SetAndroidSecret(androidSecret)
+                .SetUwpSecret(uwpSecret)
                 .Build()
                 .Should()
                 .NotBeNullOrWhiteSpace();
@@ -35,21 +43,24 @@ namespace AppCenterExtensions.Tests
         public void Build_Returns_AppCenterSecret(
             AppCenterSecretsBuilder sut,
             string appleSecret,
-            string androidSecret)
+            string androidSecret,
+            string uwpSecret)
             => sut
                 .SetAppleSecret(appleSecret)
                 .SetAndroidSecret(androidSecret)
+                .SetUwpSecret(uwpSecret)
                 .Build()
                 .Should()
-                .Be($"ios={appleSecret};android={androidSecret}");
+                .Be($"ios={appleSecret};android={androidSecret};uwp={uwpSecret};");
 
         [Theory, AutoMoqData]
         public void Static_Build_Returns_AppCenterSecret(
             string appleSecret,
-            string androidSecret)
+            string androidSecret,
+            string uwpSecret)
             => AppCenterSecretsBuilder
-                .Build(appleSecret, androidSecret)
+                .Build(appleSecret, androidSecret, uwpSecret)
                 .Should()
-                .Be($"ios={appleSecret};android={androidSecret}");
+                .Be($"ios={appleSecret};android={androidSecret};uwp={uwpSecret};");
     }
 }
--- src/AppCenterExtensions/AppCenterSecretsBuilder.cs
+++ src/AppCenterExtensions/AppCenterSecretsBuilder.cs
@@ -10,6 +10,7 @@ namespace AppCenterExtensions
     {
         private string appleSecret;
         private string androidSecret;
+        private string uwpSecret;
 
         /// <summary>
         /// Set the API secret for the iOS app
@@ -34,6 +35,17 @@ namespace AppCenterExtensions
         }
 
         /// <summary>
+        /// Sets the API secret for the UWP apps
+        /// </summary>
+        /// <param name="secret">UWP secret</param>
+        /// <returns>Returns self (Fluent API)</returns>
+        public AppCenterSecretsBuilder SetUwpSecret(string secret)
+        {
+            uwpSecret = secret ?? throw new ArgumentNullException(nameof(secret));
+            return this;
+        }
+
+        /// <summary>
         /// Builds a single AppCenter API secret string
         /// </summary>
         /// <returns>This returns a string like ios={secret};android={secret}</returns>
@@ -45,7 +57,10 @@ namespace AppCenterExtensions
                 builder.Append($"ios={appleSecret};");
             
             if (!string.IsNullOrWhiteSpace(androidSecret))
-                builder.Append($"android={androidSecret}");
+                builder.Append($"android={androidSecret};");
+            
+            if (!string.IsNullOrWhiteSpace(uwpSecret))
+                builder.Append($"uwp={uwpSecret};");
             
             return builder.ToString();
         }
@@ -55,11 +70,13 @@ namespace AppCenterExtensions
         /// </summary>
         /// <param name="appleSecret">iOS secret</param>
         /// <param name="androidSecret">Android secret</param>
+        /// <param name="uwpSecret">UWP secret</param>
         /// <returns>This returns a string like ios={secret};android={secret}</returns>
-        public static string Build(string appleSecret, string androidSecret)
+        public static string Build(string appleSecret, string androidSecret, string uwpSecret)
             => new AppCenterSecretsBuilder()
                 .SetAppleSecret(appleSecret)
                 .SetAndroidSecret(androidSecret)
+                .SetUwpSecret(uwpSecret)
                 .Build();
     }
 }
